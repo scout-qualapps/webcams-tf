0630a648e314d62ad5951b47988d509752ab964c
Joe Puli <jpuli@users.noreply.github.com>
2025-10-27 11:21:06 -0700

    Add DBT template file: dbt/models/datamart/model_dm.sql

---
+++ dbt/models/datamart/model_dm.sql
@@ -0,0 +1,13 @@
+WITH source_data AS (
+    SELECT 
+        id,
+        name,
+        created_at
+    FROM {{ source('raw', 'users') }}
+)
+SELECT 
+    id,
+    name,
+    created_at
+FROM source_data
+WHERE created_at >= '2024-01-01'
